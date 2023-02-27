98cb6671a860dfa78e286b4d177777247e9bbf80
TSanni <tomas.sanni@gmail.com>
2023-02-26 21:54:16 -0600

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Tomas Sanni on 2/26/23.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
